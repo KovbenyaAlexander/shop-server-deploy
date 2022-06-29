ef81555ce539985709bf656cfcf493e72fd009d9
KovbenyaAlexander <kovbenya.alexander1@gmail.com>
2022-06-29 11:39:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://kovbenyaalexander.github.io/shop/",
+    origin: "https://kovbenyaalexander.github.io",
     credentials: true,
   })
 );
